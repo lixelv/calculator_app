5af1a3eb650ade19593f1dcfa966f6544a889024
lixel_ <133605174+lixelv@users.noreply.github.com>
2024-07-29 00:57:55 +0300

    tauri added

---
+++ tauri-app/src/main.js
@@ -0,0 +1,19 @@
+import { currentMonitor } from "@tauri-apps/api/window"
+const monitor = currentMonitor()
+
+let greetInputEl
+let greetMsgEl
+
+async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = tauri_window.PhysicalSize
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    greetInputEl = document.querySelector("#greet-input")
+    greetMsgEl = document.querySelector("#greet-msg")
+    document.querySelector("#greet-form").addEventListener("submit", (e) => {
+        e.preventDefault()
+        greet()
+    })
+})
